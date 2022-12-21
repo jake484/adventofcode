7b3e31afcb6c7c399eb283cab7145be1cb172d32
yjy <522432938@qq.com>
2022-12-21 15:15:56 +0800

    finish 20

---
+++ day20.jl
@@ -0,0 +1,83 @@
+using Dates
+# 读取数据
+function readData(path="data/day20.txt")
+    Dictdata = Dict{Int64,Int64}()
+    listdata = String[]
+    open(path) do f
+        for line in eachline(f)
+            n = parse(Int64, line)
+            if haskey(Dictdata, n)
+                Dictdata[n] += 1
+            else
+                Dictdata[n] = 1
+            end
+            push!(listdata, line * "," * string(Dictdata[n]))
+        end
+    end
+    return listdata
+end
+
+# 获得移动后的坐标
+function getIndex(index::Int64, step::Int64, l::Int64)
+    index = index + step
+    if index ∈ 1:l
+        return index
+    else
+        return mod1(index , l - 1) 
+    end
+end
+
+# 判断数据是否完成移动
+function isMoved(num::Int64, moved::Dict{Int64,Int64}, data::Dict{Int64,Int64})
+    return haskey(moved, num) && moved[num] == data[num]
+end
+
+# 将数据添加到移动列表
+function addMoved!(num::Int64, moved::Dict{Int64,Int64})
+    if haskey(moved, num)
+        moved[num] += 1
+    else
+        moved[num] = 1
+    end
+end
+
+# 移动数据
+function moveData!(data::Vector{String}, moved::Vector{String})
+    l = length(data)
+    while length(moved) < l
+        for i in eachindex(data)
+            data_i = parse(Int64, split(data[i], ",")[1])
+            if data[i] in moved
+                continue
+            end
+            if data_i == 0
+                push!(moved, data[i])
+            else
+                push!(moved, data[i])
+                d = data[i]
+                index = getIndex(i, data_i, l)
+                if i < index
+                    data[i:index-1] .= data[i+1:index]
+                    data[index] = d
+                elseif i == index
+                    nothing
+                else
+                    data[index+1:i] .= data[index:i-1]
+                    data[index] = d
+                end
+            end
+            break
+        end
+        println("Now: $(now()),MOVEED:$(length(moved))")
+        # println(data)
+    end
+end
+
+listdata = readData()
+moved = String[]
+moveData!(listdata, moved)
+
+zeroindex = findfirst(x -> x[1] == '0', listdata)
+l = length(listdata)
+sum(x -> parse(Int64, split(x, ",")[1]), listdata[mod1(zeroindex + i, l)] for i in [1000, 2000, 3000])
+
---
+++ day20_.jl
@@ -0,0 +1,79 @@
+using Pipe
+
+parse_input(fpath="data/day20.txt") = @pipe readlines(fpath) .|> parse(Int16, _)
+example_input = Int16[1, 2, -3, 3, -2, 0, 4]
+
+mutable struct WrappyVector{N,T}
+    values::Vector{T}
+    successors::Vector{T}
+    predecessors::Vector{T}
+    zero_idx::T
+    function WrappyVector(values::Vector{T}) where {T}
+        N = length(values)
+        successors = T.(mod1.((2:(N+1)), N))
+        predecessors = T.(mod1.((N:(2N-1)), N))
+        zero_idx = findfirst(v -> v == 0, values)
+        new{N,T}(values, successors, predecessors, zero_idx)
+    end
+end
+
+s_or_p(w::WrappyVector, s) = (s < 0) ? w.predecessors : w.successors
+neighbours(w, i) = (w.predecessors[i], w.successors[i])
+
+function jump(vector, i, n_jumps)
+    for _ ∈ 1:n_jumps
+        i = vector[i]
+    end
+    i
+end
+
+function shift!(w::WrappyVector{N,T}) where {N,T}
+    for (i, v) ∈ enumerate(w.values)
+        v == 0 && continue
+        s = sign(v)
+        m = abs(v)
+        side = s_or_p(w, s)
+        rem_shift = m % (N - 1)
+        j = jump(side, i, rem_shift)
+        p_i, s_i = neighbours(w, i)
+        p_j, s_j = neighbours(w, j)
+        w.successors[p_i] = s_i
+        w.predecessors[s_i] = p_i
+        if (s > 0)
+            w.successors[j] = i
+            w.predecessors[i] = j
+            w.successors[i] = s_j
+            w.predecessors[s_j] = i
+        else
+            w.successors[p_j] = i
+            w.predecessors[i] = p_j
+            w.successors[i] = j
+            w.predecessors[j] = i
+        end
+    end
+end
+
+function grove_coordinates(w)
+    a = jump(w.successors, w.zero_idx, 1000)
+    b = jump(w.successors, a, 1000)
+    c = jump(w.successors, b, 1000)
+    (a, b, c) .|> i -> w.values[i]
+end
+
+function solve_p1(nums)
+    w = WrappyVector(nums)
+    shift!(w)
+    grove_coordinates(w) |> sum
+end
+
+parse_input() |> solve_p1 |> println
+
+function solve_p2(nums, decription_key=811589153)
+    w = WrappyVector(nums * decription_key)
+    for _ ∈ 1:10
+        shift!(w)
+    end
+    grove_coordinates(w) |> sum
+end
+
+parse_input() |> solve_p2 |> println
