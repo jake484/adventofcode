25af0a8f755d351a6d25eee1df0a05b344d32a26
yjy <522432938@qq.com>
2023-12-13 22:44:11 +0800

    update 12

--- 2023/day12.jl
+++ 2023/day12.jl
@@ -7,7 +7,11 @@ function readData(path, ::Val{12})
     end
     return chars, nums
 end
-
+function getRegex(nums::Vector{Int})
+    rnums = map(n -> join(("([?]+)([?#]{0,", n, "}?)([?]+)")), nums)
+    rstring = join(rnums, "([?.]{1,}?)")
+    return Regex(join(("^[.]*", rstring, "[.]*\$")))
+end
 function partOne(data)
     return 0
 end
@@ -23,9 +27,15 @@ end
 
 # test
 data = readData("data/2023/day12.txt", Val(12))
-day12_main()
+# day12_main()
 
 # using BenchmarkTools
 # @info "day12 性能："
 # @btime day12_main()
+# getRegex([3, 2, 1])
+# chars, nums = data
+# for (cs, ns) in zip(chars, nums)
+#     match(getRegex(ns), join(cs)).captures |> println
+# end
 
+match(getRegex([6, 2, 1, 1]), "??#?#?#????......????#?").captures
