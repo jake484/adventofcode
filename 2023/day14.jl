dc28cf6983f7e0aef4f208c573f6d8bb7f7c78da
yjy <522432938@qq.com>
2023-12-14 19:57:07 +0800

    2023-day14

--- 2023/day14.jl
+++ 2023/day14.jl
@@ -18,14 +18,13 @@ function findToMove(col, index)
     return toMove
 end
 
-function partOne(data, ::Val{T}) where {T}
-    type = T == 1 || T == 3 ? eachcol(data) : eachrow(data)
+function moveOneTime(data, ::Val{T}) where {T}
     if T == 3
         reverse!(data, dims=1)
-        reverse
     elseif T == 4
         reverse!(data, dims=2)
     end
+    type = T == 1 || T == 3 ? eachcol(data) : eachrow(data)
     for col in type
         rock = 0
         toMove = findToMove(col, rock)
@@ -38,23 +37,57 @@ function partOne(data, ::Val{T}) where {T}
             end
         end
     end
+    if T == 3
+        reverse!(data, dims=1)
+    elseif T == 4
+        reverse!(data, dims=2)
+    end
+    return 0
+end
+
+function partOne(data)
+    moveOneTime(data, Val(1))
     return sum(x -> size(data, 1) - x.I[1] + 1, findall(x -> x == 'O', data))
 end
 
+function has_cycle(arr)
+    for i in 1:length(arr)
+        if arr == circshift(arr, i)
+            return i
+        end
+    end
+    return -1
+end
+
 function partTwo(data)
-    return 0
+    arr = Int[]
+    for _ in 1:200
+        for i in 1:4
+            moveOneTime(data, Val(i))
+        end
+        res = sum(x -> size(data, 1) - x.I[1] + 1, findall(x -> x == 'O', data))
+        push!(arr, res)
+    end
+    len = 1
+    str = join(arr)
+    while len < length(arr)
+        occursin(repeat(join(arr[101:101+len]), 2), str) && break
+        len += 1
+    end
+    return arr[(1000000000-100)%(len+1)+100]
 end
 
 function day14_main()
     data = readData("data/2023/day14.txt", Val(14))
-    return partOne(data), partTwo(data)
+    return partOne(copy(data)), partTwo(data)
 end
 
 # test
-data = readData("data/2023/day14.txt", Val(14))
-day14_main()
+# data = readData("data/2023/day14.txt", Val(14))
+# day14_main()
 
-# using BenchmarkTools
-# @info "day14 性能："
-# @btime day14_main()
+using BenchmarkTools
+@info "day14 性能："
+@btime day14_main()
+
 
--- temp.md
+++ temp.md
@@ -1,1 +1,1 @@
-|    day13 | :white_check_mark: | :white_check_mark: |  5.334 ms |      2.49 MiB |  [link](https://github.com/jake484/adventofcode/blob/master/2023/day13.jl)  |
+|    day14 | :white_check_mark: | :white_check_mark: |  27.971 ms |      7.59 MiB |  [link](https://github.com/jake484/adventofcode/blob/master/2023/day14.jl)  |
