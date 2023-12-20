cc3f5cbc5a547f7642885e5bb7a53d2422c64d3d
yjy <522432938@qq.com>
2023-12-20 21:27:29 +0800

    update20

--- 2023/day20.jl
+++ 2023/day20.jl
@@ -1,77 +1,118 @@
 mutable struct FlipFlop
     state::Bool
+    sendTo::Vector{String}
+    receiveFrom::Dict{String,Bool}
+    recentPulse::Bool
     function FlipFlop()
-        new(false)
+        new(false, String[], Dict{String,Bool}(), false, false)
+    end
+end
+
+mutable struct Broadcaster
+    state::Bool
+    sendTo::Vector{String}
+    receiveFrom::Dict{String,Bool}
+    recentPulse::Bool
+    function Broadcaster()
+        new(false, String[], Dict{String,Bool}(), false, false)
     end
 end
 
 mutable struct Conjunction
     state::Bool
+    sendTo::Vector{String}
+    receiveFrom::Dict{String,Bool}
+    recentPulse::Bool
     function Conjunction()
-        new(false)
+        new(false, String[], Dict{String,Bool}(), false, false)
     end
 end
 mutable struct Output end
 
-function process(pulse::Bool, ff::FlipFlop, outputToInput, name)
-    if !pulse
+function process(ff::FlipFlop, name::String)
+    sendPluse = false
+    if !ff.recentPulse
         ff.state = !ff.state
-        return ff.state
+        sendPluse = ff.state
     end
-    return nothing
+    sendPluse = nothing
+    for receiver in ff.sendTo
+        eval(Symbol(receiver)).receiveFrom[name] = sendPluse
+        eval(Symbol(receiver)).recentPulse = sendPluse
+    end
+    return sendPluse, length(ff.sendTo)
 end
 
-process(pulse::Bool, e::Output, outputToInput, name) = return nothing
+process(e::Output, name) = nothing
 
-function process(pulse::Bool, c::Conjunction, outputToInput, name)
-    c.state = pulse
-    return !all(x -> eval(Symbol(x)).state == true, outputToInput[name])
+function process(b::Broadcaster, name::String)
+    sendPluse = false
+    for receiver in ff.sendTo
+        eval(Symbol(receiver)).receiveFrom[name] = sendPluse
+        eval(Symbol(receiver)).recentPulse = sendPluse
+    end
+    return sendPluse, length(b.sendTo)
+end
+
+function process(c::Conjunction, name::String)
+    c.state = c.recentPulse
+    sendPluse = !all(values(c.receiveFrom))
+    for receiver in c.sendTo
+        eval(Symbol(receiver)).receiveFrom[name] = sendPluse
+        eval(Symbol(receiver)).recentPulse = sendPluse
+    end
+    return sendPluse, length(c.sendTo)
 end
 
 function readData(path, ::Val{20})
-    inputToOutput, names = Dict{String,Vector{String}}(), String[]
-    outputToInput = Dict{String,Vector{String}}()
+    names = String[]
+    receiverBuffer = Pair{String,String}[]
     for line in eachline(path)
         name, distination = split(line, " -> ")
         if name[1] == '%'
-            name = String(name[2:end])
-            push!(names, name)
+            name = name[2:end]
             eval(Meta.parse(join(("const ", name, "=FlipFlop()"))))
         elseif name[1] == '&'
-            name = String(name[2:end])
-            push!(names, name)
+            name = name[2:end]
             eval(Meta.parse(join(("const ", name, "=Conjunction()"))))
+        else
+            eval(Meta.parse(join(("const ", name, "=Broadcaster()"))))
         end
-        inputToOutput[name] = map(String, split(distination, ", "))
-        for output in inputToOutput[name]
-            if haskey(outputToInput, output)
-                push!(outputToInput[output], name)
+        push!(names, String(name))
+        senders = map(String, split(distination, ", "))
+        eval(Symbol(name)).sendTo = senders
+        for receiver in senders
+            if receiver ∈ names
+                eval(Symbol(receiver)).receiveFrom[name] = false
             else
-                outputToInput[output] = [name]
+                push!(receiverBuffer, (receiver => name))
             end
         end
     end
-    allName = reduce(vcat, values(inputToOutput))
-    for name in setdiff(allName, names)
-        eval(Meta.parse(join(("const ", name, "=Output()"))))
+    while !isempty(receiverBuffer)
+        (receiver, sender) = pop!(receiverBuffer)
+        if receiver ∈ names
+            eval(Symbol(receiver)).receiveFrom[sender] = false
+        else
+            eval(Meta.parse(join(("const ", receiver, "=Output()"))))
+        end
     end
-    return inputToOutput, outputToInput, names
+    return names
 end
 
 getAllStates(names) = map(x -> eval(Symbol(x)).state, names)
 
-function pushingButtonOnce(inputToOutput, outputToInput)
-    nextNames = inputToOutput["broadcaster"]
-    nextPulses = map(x -> process(false, eval(Symbol(x)), outputToInput, "broadcaster"), nextNames)
-    # println("nextNames: ", nextNames, " nextPulses: ", nextPulses)
+function pushingButtonOnce(senders, receivers)
+    nextNames = broadcaster.sendTo
+    nextPulses = map(x -> process(eval(Symbol(x)), "broadcaster")[1], nextNames)
     nfalses, ntrues = 1 + length(nextPulses), 0
     while any(!isnothing, nextPulses)
         ps = Union{Bool,Nothing}[]
         ns = String[]
         for (name, pulse) in zip(nextNames, nextPulses)
             if !isnothing(pulse)
-                append!(ns, inputToOutput[name])
-                p = map(x -> process(pulse, eval(Symbol(x)), outputToInput, x), inputToOutput[name])
+                append!(ns, senders[name])
+                p = map(x -> process(pulse, eval(Symbol(x)), receivers, x), senders[name])
                 if pulse
                     ntrues += length(p)
                 else
@@ -91,11 +132,11 @@ function pushingButtonOnce(inputToOutput, outputToInput)
 end
 
 function partOne(data)
-    inputToOutput, outputToInput, names = data
+    senders, receivers, names = data
     times = 0
     nfalses, ntrues = 0, 0
-    for _ in 1:1000
-        nf, nt = pushingButtonOnce(inputToOutput, outputToInput)
+    for _ in 1:1
+        nf, nt = pushingButtonOnce(senders, receivers)
         nfalses, ntrues = nfalses + nf, ntrues + nt
         times += 1
         # if all(==(false), getAllStates(names))
@@ -116,8 +157,8 @@ function day20_main()
 end
 
 # test
-# data = readData("data/2023/day20.txt", Val(20))
-day20_main()
+data = readData("data/2023/day20.txt", Val(20))
+# day20_main()
 
 # using BenchmarkTools
 # @info "day20 性能："
