bec5e8ff88d1335f6e07de9942813bf0c31e1136
jake484 <522432938@qq.com>
2024-06-12 15:40:20 +0800

    update

--- 2023/day25.jl
+++ 2023/day25.jl
@@ -3,7 +3,7 @@ function readData(path, ::Val{25})
     components = Dict{String,Int}()
     no = 1
     for line in readlines(path)
-        println(line)
+        # println(line)
         name, lists = split(line, ": ")
         data[String(name)] = map(x -> String(x), split(lists, " "))
         if !haskey(components, String(name))
@@ -33,36 +33,32 @@ function partOne(gp)
     es = collect(edges(gp))
     len = length(es)
     for i in 1:len
-        for j in i+1:len
-            e1, e2 = es[i], es[j]
-            rem_edge!(gp, e1)
-            rem_edge!(gp, e2)
-            cc = connected_components(gp)
-            if length(cc) == 2
-                prod(map(x -> length(x), cc)) |> println
-            end
+        e1 = es[i]
+        rem_edge!(gp, e1)
+        if length(connected_components(gp)) > 2
             add_edge!(gp, e1)
+            continue
+        end
+        for j in i+1:len
+            e2 = es[j]
+            rem_edge!(gp, e2)
+            if length(connected_components(gp)) > 2
+                add_edge!(gp, e2)
+                continue
+            end
+            for k in j+1:len
+                e3 = es[k]
+                rem_edge!(gp, e3)
+                cc = connected_components(gp)
+                if length(cc) == 2
+                    prod(map(x -> length(x), cc)) |> println
+                end
+                add_edge!(gp, e3)
+            end
             add_edge!(gp, e2)
         end
+        add_edge!(gp, e1)
     end
-    # for i in 1:len
-    #     for j in i+1:len
-    #         for k in j+1:len
-    #             es = collect(edges(gp))
-    #             e1, e2, e3 = es[i], es[j], es[k]
-    #             rem_edge!(gp, e1)
-    #             rem_edge!(gp, e2)
-    #             rem_edge!(gp, e3)
-    #             cc = connected_components(gp)
-    #             if length(cc) == 2
-    #                 prod(map(x -> length(x), cc)) |> println
-    #             end
-    #             add_edge!(gp, e1)
-    #             add_edge!(gp, e2)
-    #             add_edge!(gp, e3)
-    #         end
-    #     end
-    # end
     return 0
 end
 
